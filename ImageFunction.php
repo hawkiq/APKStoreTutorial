927be5aa41e52f409563c3833e159ca993620e02
hawkiq <hawkiq@users.noreply.github.com>
2017-09-15 16:15:18 +0300

    Added description for each operation

--- ImageFunction.php
+++ ImageFunction.php
@@ -1,3 +1,4 @@
+<?php
 function create_thumb($img_name,$newname,$type,$width,$height){
     if(!strcmp("image/jpg",$type) || !strcmp("image/jpeg",$type) || !strcmp("image/pjpeg",$type))
         $src_img=imagecreatefromjpeg($img_name);
@@ -8,9 +9,11 @@ function create_thumb($img_name,$newname,$type,$width,$height){
     if(!strcmp("image/gif",$type))
         $src_img=imagecreatefromgif($img_name);
 
+    // اخذ ابعاد الصورة المرفوعة
     $old_x=imageSX($src_img);
     $old_y=imageSY($src_img);
 
+    // التاكد من عرض ةارتفاع الصورة وتحويلها الى الاترفاع والعرض المدخلة مع الفنكشن
     if ($old_x > $width ){
         $new_x = $width;
     }else{
@@ -22,14 +25,17 @@ function create_thumb($img_name,$newname,$type,$width,$height){
         $new_y = $height;
     }
 
+    // صنع صورة جديدة بنفس الابعاد المطلوبة
     $dst_img=ImageCreateTrueColor($new_x,$new_y);
     imagesavealpha($dst_img, true);
     $color = imagecolorallocatealpha($dst_img, 0, 0, 0, 127);
     imagefill($dst_img, 0, 0, $color);
     imagecopyresampled($dst_img,$src_img,0,0,0,0,$new_x,$new_y,$old_x,$old_y);
 
+    //مسار ملف الشعار الخاص بالموقع
     $watermark_file='images/watermark.gif';
 
+    //درجة شفافية الشعار على الصورة المرفوعة
     $transparency = 20;
 
     $wext=getFileExtension($watermark_file);
@@ -44,13 +50,15 @@ function create_thumb($img_name,$newname,$type,$width,$height){
     $watermark_width = imagesx($watermark);
     $watermark_height = imagesy($watermark);
 
-    // place watermark image on the right left of the main image
+    // لجعل الشعار اسفل يمين الصورة قم بالغاء التعليق من السطرين التاليين ووضع علامه / اما السطرين الاحقين
     //$dest_x = $old_x - $watermark_width - 5;
     //$dest_y = $old_y - $watermark_height - 5;
 
+    // لجعل الشعار في منتصف الصورة المرفوعة
     $dest_x = (($new_x - $watermark_width)/2);
     $dest_y = (($new_y - $watermark_height)/2);
 
+    // دمج صورة الشعار مع الصورة المرفوعة
     imagecopymerge($dst_img, $watermark, $dest_x, $dest_y, 0, 0, $watermark_width, $watermark_height, $transparency);
 
     if(!strcmp("image/png",$type))  imagepng($dst_img,$newname);
@@ -60,6 +68,7 @@ function create_thumb($img_name,$newname,$type,$width,$height){
     imagedestroy($dst_img);
     imagedestroy($src_img);
 }
+//فنكشن لمعرفة امتداد الملف
 function getFileExtension($str) {
     $i = strrpos($str,".");
     if (!$i) { return ""; }
@@ -67,3 +76,5 @@ function getFileExtension($str) {
     $ext = substr($str,$i+1,$l);
     return $ext;
 }
+
+
